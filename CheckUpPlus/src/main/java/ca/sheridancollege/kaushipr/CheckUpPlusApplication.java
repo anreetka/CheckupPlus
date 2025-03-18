d1e583e6e28042aeea8af5bd66fe35072b6b2be4
Priyanshu Kaushik <kaushipr@sheridancollege.ca>
2025-03-18 11:52:40 -0400

    spring setup

---
+++ CheckUpPlus/src/main/java/ca/sheridancollege/kaushipr/CheckUpPlusApplication.java
@@ -0,0 +1,13 @@
+package ca.sheridancollege.kaushipr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckUpPlusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckUpPlusApplication.class, args);
+	}
+
+}
