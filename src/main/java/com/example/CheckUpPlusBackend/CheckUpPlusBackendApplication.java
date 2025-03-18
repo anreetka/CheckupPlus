d3a70899eec1fa809282ab3d70691c63d9cbdfc8
= <=>
2025-03-18 10:22:21 -0400

    Initial project setup

---
+++ src/main/java/com/example/CheckUpPlusBackend/CheckUpPlusBackendApplication.java
@@ -0,0 +1,12 @@
+package com.example.CheckUpPlusBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckUpPlusBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckUpPlusBackendApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/CheckUpPlusBackend/CheckUpPlusBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CheckUpPlusBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheckUpPlusBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
